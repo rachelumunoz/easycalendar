1ebb35662a42562203aeaeabc137c285b0cbb441
Clara Bova <clarambova@gmail.com>
2016-09-30 11:46:08 -0700

    Add omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930023630) do
+ActiveRecord::Schema.define(version: 20160930182257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
