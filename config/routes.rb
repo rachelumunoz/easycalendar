7e1c0d03835d3f08f1f59b1c2e023d1800759c38
Andrew Palmes <email": null>
2016-09-30 13:48:30 -0700

    Reset Google secret

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-GoogleAuthExample::Application.routes.draw do
+Easycalendar::Application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
