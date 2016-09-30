03549f2c08cd13b9f073b95b3e8bfd1da10ce23b
Rachel Munoz <email": null>
2016-09-30 15:38:24 -0700

    Adds figaro and ENV variables for client id and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '6889202643-emivcl25lr5805gdeq7rn86pno6241em.apps.googleusercontent.com', 'SECRET', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
