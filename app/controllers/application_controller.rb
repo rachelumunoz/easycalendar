73ce6b2897de70659fb4ba1ee432e3be4b94afc9
Rachel Munoz <email": null>
2016-10-03 11:23:05 -0700

    Removes extra sign in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  # def after_sign_in_path_for(resource_or_scope)
-  #  if request.env['omniauth.origin']
-  #     request.env['omniauth.origin']
-  #   end
-  # end
 end
