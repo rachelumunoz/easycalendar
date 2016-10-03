1e474935ba275434048b6b754417a067aed6f6d7
Clara Bova <email": null>
2016-10-03 13:50:33 -0700

    Debugging user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
   has_many :children, foreign_key: 'parent_id'
+  has_many :clients_children, through: :clients, source: :children
 
   has_many :appointments, through: :children
+  has_many :coached_appointments, through: :coach_activities, source: :appointments
 
   has_many :coach_activities, foreign_key: :coach_id
   has_many :activities, through: :coach_activities
 
-  has_many :coached_appointments, through: :coach_activities, source: :appointments
-
   has_many :client_locations, foreign_key: :client_id
   has_many :coach_locations, foreign_key: :coach_id
+
   has_many :coaches, through: :appointments
   has_many :clients, through: :appointments
-  has_many :clients_children, through: :clients, source: :children
+
   has_many :notification_receivers, foreign_key: 'receiver_id'
   has_many :notifications_received, through: :notification_receivers, source: :notification
   has_many :notifications_sent, through: :appointments, source: :notifications
+
   has_many :messages_received, class_name: "Message", foreign_key: :receiver_id
   has_many :messages_sent, class_name: "Message", foreign_key: :sender_id
 
@@ -30,6 +32,10 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
+  end
+
+  def full_name
+    self.first_name + " " + self.last_name
   end
 
 end
