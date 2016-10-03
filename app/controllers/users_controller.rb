9dd20f9fe1e565fb071f7d9ef913cf2824b6cebd
Clara Bova <email": null>
2016-10-03 13:59:32 -0700

    Update user controller, views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    @testuser = User.find(6)
-    @appointments = @testuser.appointments
-    @c_appointments = @testuser.coached_appointments
+    @user = current_user
+    # @testuser = User.find(6)
+    # @appointments = @testuser.appointments
+    # @c_appointments = @testuser.coached_appointments
   end
 
 end
