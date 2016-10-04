7925b1bc01b1390f70a6a01183f03d44cb10eb4b
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-10-04 12:52:00 -0700

    Fixes associations between parents and coaches

--- app/models/coach_activity.rb
+++ app/models/coach_activity.rb
@@ -2,6 +2,8 @@ class CoachActivity < ApplicationRecord
   belongs_to :coach, class_name: "User"
   belongs_to :activity
   has_many :appointments
+  has_many :invites
+  has_many :clients, through: :invites
 
   validates :coach, presence: true
   validates :activity, presence: true
