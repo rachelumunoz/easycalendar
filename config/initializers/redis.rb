34da0d6b18bbc70fa2910ba993d35a400b4e6147
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-10-05 11:30:28 -0700

    Use consistent env var for redis url, output the redis object on initialization

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 uri = URI.parse(ENV["REDIS_URL"] || "redis://localhost:6379/" )
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+puts $redis.inspect
 REDIS = $redis
