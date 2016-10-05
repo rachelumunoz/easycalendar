9bedff6b87b6c63334c3085e124b7ae2d66dc949
Ian Lucas <ianlucas1@MacBook-Pro-2.local>
2016-10-05 11:21:32 -0700

    Create REDIS constant, attempting to get correct url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+uri = URI.parse(ENV["REDIS_URL"] || "redis://localhost:6379/" )
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+REDIS = $redis
