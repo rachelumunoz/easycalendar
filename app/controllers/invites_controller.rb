a21f8a9471b8d5eb888a04c2f92a63f158ee95f0
Andrew Palmes <email": null>
2016-10-05 16:22:52 -0700

    Baseline user experience complete

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,27 +1,31 @@
 class InvitesController < ApplicationController
   def new
-    @user = User.find(3)
+    @user = current_user
     @new_user = User.new
+    @new_child = Child.new
     @invite = Invite.new
   end
 
   def create
-    # user that is logged in, replace w/ current_user when fully implemented
-    @user = User.find(3)
+
+    @user = current_user
     @new_user = User.new
     @new_user.email = params[:invite][:user][:email]
     @new_user.phone_number = params[:invite][:user][:phone_number]
     @new_user.password = "password"
     @new_user.save
-    p @new_user
+
+    @new_child = Child.new
+    @new_child.first_name = params[:invite][:child][:first_name]
+    @new_child.last_name = params[:invite][:child][:last_name]
+    @new_child.age = params[:invite][:child][:age].to_i
+    @new_child.parent_id = @new_user.id
+    @new_child.save
 
     @invite = Invite.new
     @invite.coach_activity_id = params[:invite][:coach_activity_id].to_i
     @invite.client_id = @new_user.id
-    p "pre-save----------2:(>.<)----------"
-    p @invite
     @invite.save
-    p "post-save----------2:(^_^)----------"
     redirect_to profile_path
   end
 
